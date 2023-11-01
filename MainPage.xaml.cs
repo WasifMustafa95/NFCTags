79858cdac9aeb015b911a1db8370503a5f356c22
WasifMustafa95 <55859752+WasifMustafa95@users.noreply.github.com>
2023-11-02 04:05:59 +0500

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿namespace NFCTagApp
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+#if IOS
+            NFCTagApp.Platforms.iOS.NFCTagService nFCTagService = new NFCTagApp.Platforms.iOS.NFCTagService();
+            nFCTagService.Scan();
+#endif
+        }
+    }
+}
